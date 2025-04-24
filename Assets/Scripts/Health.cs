63307712195d83594d95878c04d954c3353427fc
DoG3312 <din_din2019@mail.ru>
2025-04-24 20:46:28 +0500

    HealthBar2
    -Перевел из слайдоров на Image
    -Добавил зеленую полоску хп
    -Теперь на любых экранах выглядит хорошо

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,23 +5,33 @@ public class Health : MonoBehaviour
 {
     public float MaxHP = 100f;
     public float HP = 100;
-    public float learpSpeed = 0.05f;
-    [Header("Sliders")]
-    public Slider HP_Slider;
-    public Slider easeHealthBar;
+    private float fillAmount_HP = 0;
+
+    [Header("lerpSpeeds")]
+    public float lerpSpeed_Red = 0.4f;
+    public float reducedSpeed_Red = 0.18f;
+    public float lerpSpeed_Yellow = 1;
+    [Header("Image")]
+    public Image Red_HP;
+    public Image Yellow_HP;
+    public Image Green_HP;
+
     void Start()
     {
-        HP_Slider.maxValue = MaxHP;
-        easeHealthBar.maxValue = MaxHP;
-        HP_Slider.value = MaxHP;
+        Red_HP.fillAmount = HP / MaxHP;
     }
 
     void Update()
     {
+        fillAmount_HP = HP / MaxHP;
 
-        if (HP_Slider.value != easeHealthBar.value) 
+        float lerpTime = 0f;
+
+        if (Green_HP.fillAmount != Red_HP.fillAmount)
         {
-            easeHealthBar.value = Mathf.Lerp(easeHealthBar.value, HP,easeHealthBar.value > HP ? learpSpeed : 1);
+            lerpTime += Time.deltaTime;
+            Red_HP.fillAmount = Mathf.Lerp(Red_HP.fillAmount, fillAmount_HP, Red_HP.fillAmount > Green_HP.fillAmount ? lerpTime / reducedSpeed_Red : lerpTime / lerpSpeed_Red);
+            Yellow_HP.fillAmount = Mathf.Lerp(Yellow_HP.fillAmount, fillAmount_HP, Yellow_HP.fillAmount > Red_HP.fillAmount ? lerpTime / lerpSpeed_Yellow : 1);
         }
 
         if (Input.GetKeyUp(KeyCode.E)) //для проверки
@@ -37,19 +47,18 @@ public class Health : MonoBehaviour
     public void TakeDamage(float damage)
     {
         HP -= damage;
-        HP_Slider.value = HP;
+        Green_HP.fillAmount = HP / MaxHP;
     }
 
-    public void Hieal(float heal)
+    public void Hieal(float healingAmount)
     {
-        if (HP + heal >= MaxHP) 
+        if (HP + healingAmount >= MaxHP) 
         {
             HP = MaxHP;
-            HP_Slider.value = HP;
+            Green_HP.fillAmount = HP / MaxHP;
             return;
         }
-        HP += heal;
-        HP_Slider.value = HP;
+        HP += healingAmount;
+        Green_HP.fillAmount = HP / MaxHP;
     }
-
 }
