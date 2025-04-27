3e5b42172ce487225819103e1e360381c7db0e25
DoG3312 <din_din2019@mail.ru>
2025-04-27 20:12:15 +0500

    СreateEnemy
    -Поправил

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -3,43 +3,18 @@ using UnityEngine.AI;
 
 public class BaseEnemy : MonoBehaviour
 {
-    [Header("EnemyStats")]
     public float rotationSpeed = 45f;
-    public float lookRadius = 10;
-    public float enemyDamage = 10f;
 
-    private NavMeshAgent nma;
-    private Animator animator;
-    private Transform player;
-    private bool isAtac = false;
+    public NavMeshAgent agent;
+    [SerializeField] EnemyAttackTrigger enemyAttackTrigger;
+
 
     private void Start()
     {
-        nma = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
-        player = FindFirstObjectByType<PlayerMovement>().transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
-    private void Update()
-    {
-        float distance = Vector3.Distance(player.position, transform.position);
-        if(distance <= lookRadius)
-        {
-            nma.SetDestination(player.position);
-        }
-
-        if (isAtac)
-        {
-            nma.isStopped = true;
-            LookAtPlayer(player);
-        }
-        else
-        {
-            nma.isStopped = false;
-        }
-    }
-
-    private void LookAtPlayer(Transform target) //поворачивает с сторону таргета
+    public void LookAtTarget(Transform target) //поворачивает с сторону таргета
     {
         Vector3 targetDirection = target.position - transform.position;
         targetDirection.y = 0;
@@ -47,32 +22,16 @@ public class BaseEnemy : MonoBehaviour
         transform.rotation = Quaternion.Slerp(transform.rotation, targetEnemyRotation, Time.deltaTime * rotationSpeed);
     }
 
-    private void OnTriggerEnter(Collider other) 
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            isAtac = true;
-            animator.SetBool("IsAtaking", isAtac);
+            agent.SetDestination(other.transform.position);
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    public void Attack()
     {
-        if (other.CompareTag("Player"))
-        {
-            isAtac = false;
-            animator.SetBool("IsAtaking", isAtac);
-        }
-    }
-
-    private void OnDrawGizmos() 
-    {
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, lookRadius);
-    }
-
-    public void EnemyAtak()
-    {
-        player.GetComponent<Health>().TakeDamage(enemyDamage);
+        enemyAttackTrigger.EnemyAttack();
     }
 }
--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -2,32 +2,17 @@ using UnityEngine;
 
 public class EnemyHealth : Health
 {
-    private GameObject enemyCanvas;
+    [SerializeField] private GameObject enemyCanvas;
 
-    private void Start()
-    {
-        enemyCanvas = GetComponentInChildren<Canvas>().gameObject;
-        enemyCanvas.SetActive(false);
-    }
     public override void TakeDamage(float damage)
     {
         enemyCanvas.SetActive(true);
         base.TakeDamage(damage);
-        if (HP <= 0)
-        {
-            Destroy(gameObject);
-        }
+        if (HP <= 0) Destroy(gameObject);
     }
-    public override void Hiealing(float healingAmount)
+    public override void ApplyHealing(float amount)
     {
-        if (HP + healingAmount >= MaxHP)
-        {
-            HP = MaxHP;
-            Green_HP.fillAmount = HP / MaxHP;
-            enemyCanvas.SetActive(false);
-            return;
-        }
-        HP += healingAmount;
-        Green_HP.fillAmount = HP / MaxHP;
+        base.ApplyHealing(amount);
+        if (HP >= MaxHP) enemyCanvas.SetActive(false);
     }
 }
