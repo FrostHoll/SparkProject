e9a66fcff9ecd653c3b4bbaf468f0cfb8cadfbf7
vanomorodellefake <53963119+vanomorodellefake@users.noreply.github.com>
2025-05-15 23:41:01 +0500

    Обновление Looting
    - Несколько изменена логика, теперь отзываются не все сундуки, а при помощи интерфейса только тот, к которому мы обращаемся
    - Теперь предметы берутся не из кода, а из ScriptableObject, в который довольно легко вписывать объекты для дропа и их вес
    - Looting теперь нужен лишь для определения случайного предмета, который выпадет
    - В основном определение того, как игрок взаимодействует с предметом определено через интерфейс, а не UnityEvent, т.к. пока я в нём не разобрался и роль для него отвёл для спавна предмета
    - Пока не реализовано выпадение объектов с врагов, мб остался какой-то мусор и классы DropItem и DropItems имеют похожие имена, а также я устал. В целом в основном хочу показать реализацию взаимодействия при помощи интерфейса, а также реализацию ScriptableObject
    (( ещё пол часа пытался понять, почему возникает ошибка "Ожидался GameObject, а был получен Object", оказывается проблема была в том, что в инспекторе UnityEvent стоял статический тип

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -1,61 +1,16 @@
-using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class Chest : MonoBehaviour
+public class Chest : MonoBehaviour, IInteractable
 {
-    [SerializeField] private int TyepOfItems;
-    [SerializeField] private bool IsOpen = false;
-    public Animator animator;
-    public Interaction inter;
-    public Looting looting = new Looting();
+    [SerializeField] private DropItems _dropItems;
+    [SerializeField] private UnityEvent<GameObject> OnOpen;
+    [SerializeField] private Animator animator;
+    private Looting _looting = new Looting();
 
-    public GameObject ItemFromChest;
-    public float pushForce = 30f;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.E)) // DEBUG
-        {
-            IsOpen = false;
-        }
-        if (inter.Interacting == true && inter.ListOfItemsInInteractionZone.Contains(gameObject.name) && IsOpen == false)
-        {
-            Interact(); // Анимка
-            HorrorJumpItem(looting.GiveMeFromItems1(looting.items1)); // ВЫброс предмета
-        }
-    }
     public void Interact()
     {
         animator.SetBool("Openning", true);
-        IsOpen = true;
-    }
-    public void HorrorJumpItem(string name)
-    {
-        GameObject item = Instantiate(ItemFromChest, transform.position, Quaternion.identity);
-        TextMeshPro textMesh = item.GetComponentInChildren<TextMeshPro>();
-        if (textMesh != null)
-        {
-            textMesh.text = name;
-        }
-
-        Vector3 randomDirection = new Vector3(
-            Random.Range(-1f, 1f),
-            Random.Range(0.5f, 1f),
-            Random.Range(-1f, 1f)
-            ).normalized;
-
-        Rigidbody rb = item.GetComponent<Rigidbody>();
-        if (rb != null)
-        {
-            rb.AddForce(randomDirection * pushForce, ForceMode.Impulse);
-        }
-
+        OnOpen.Invoke(_looting.GiveMeFromItems(_dropItems.ListItems));
     }
 }
--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections.Generic;
 
-public class Interaction : MonoBehaviour
+public class Interaction : MonoBehaviour, IInteraction
 {
     private InputSystem_Actions inputActions;
-    public List<string> ListOfItemsInInteractionZone = new List<string>();
-    public bool Interacting = false;
+
+    [SerializeField] private bool interacting = false;
+    private List<GameObject> listOfItemsInInteractionZone = new List<GameObject>();
+
+    public bool Interacting { get { return interacting; } set { interacting = value; } }
+    public List<GameObject> ListOfItemsInInteractionZone { get { return listOfItemsInInteractionZone; } set { listOfItemsInInteractionZone = value; } }
     private void Awake()
     {
         inputActions = new InputSystem_Actions();
@@ -20,41 +24,45 @@ public class Interaction : MonoBehaviour
     {
         inputActions.Player.Interacte.performed -= InteractePerformed;
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void InteractePerformed(InputAction.CallbackContext obj)
     {
-        Interacting = true;
+        if (listOfItemsInInteractionZone.Count > 0)
+        { 
+            float minDistance = float.MaxValue;
+            GameObject nearestItem = null;
+            Vector3 playerPosition = transform.position;
+
+            foreach (var item in listOfItemsInInteractionZone)
+            {
+                float distance = Vector3.Distance(playerPosition, item.transform.position);
+                if (distance < minDistance)
+                {
+                    nearestItem = item;
+                    minDistance = distance;
+                }
+            }  
+            
+
+            IInteractable interactable = nearestItem.GetComponent<IInteractable>();
+            interactable.Interact();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log("В область вошел: " + other.name);
-        ListOfItemsInInteractionZone.Add(other.name);
-        //Debug.Log("Список: " + string.Join(", ", ListOfItemsInInteractionZone));
-        if (ListOfItemsInInteractionZone.Contains(other.name))
-        {
-            Debug.Log("Всё ок");
-        }
-        else
-        {
-            Debug.Log("Всё не ок!!!");
-        }
+        ListOfItemsInInteractionZone.Add(other.gameObject);
+
     }
     private void OnTriggerExit(Collider other)
     {
-        //Debug.Log("Из области вышел: " + other.name);
-        ListOfItemsInInteractionZone.Remove(other.name);
-        //Debug.Log("Список: " + string.Join(", ", ListOfItemsInInteractionZone));
+        ListOfItemsInInteractionZone.Remove(other.gameObject);
     }
 }
+
+interface IInteraction
+{
+    bool Interacting { get; set; }
+    List<GameObject> ListOfItemsInInteractionZone { get; set; }
+}
+
